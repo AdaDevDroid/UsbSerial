60a75e119ade7e2b59b1ecad49ab672ddc48a95a
taeKrits <kritsada@ada-soft.com>
2025-01-27 10:50:26 +0700

    Add Libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,21 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("maven-publish")
+}
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "com.github.AdaDevDroid" // GitHub Username
+            artifactId = "UsbSerial" // ชื่อ Library
+            version = "1.0.0" // เวอร์ชันของ Library
+// ระบุไฟล์ .aar ที่ต้องการเผยแพร่
+            artifact("libs/UsbSerial-6.1.0.aar") {
+                extension = "aar" // ระบุชนิดไฟล์
+            }
+        }
+        repositories {
+            mavenLocal()
+        }
+    }
 }
